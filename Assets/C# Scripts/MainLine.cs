ec01f775fb7197ac04bcead5707822f3eb756268
Stratus 8419 <87164432+ECrp233@users.noreply.github.com>
2022-03-24 23:06:37 +0800

    Add files via upload

--- Assets/C# Scripts/MainLine.cs
+++ Assets/C# Scripts/MainLine.cs
@@ -3,111 +3,150 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
-public class MainLine : MonoBehaviour
+namespace MainLine
 {
-    public GameObject CameraBind;
-    public float CameraFollowingExtent = 25.0f;
-    public Vector3 CameraBindEulerAngles = new Vector3(35, 30, 0);
-    public Vector3 CameraBindPosition = Vector3.zero;
-    public bool CameraFollow;
-    public float FollowingNeedtime;
-
-    public float Speed = 15.0f;
-    public AudioSource Song;
-
-    public Vector3 TurnAround1 = new Vector3(0, 90, 0), TurnAround2 = new Vector3(0, 0, 0);
-    public bool TurnToTA1 = true;
-
-    public GameObject LineTail;
-    private GameObject nowTail;
-
-    private bool nowPlaying = false;
-
-
-    void TurnBlock()
+    public class MainLine : MonoBehaviour
     {
-        if(TurnToTA1)
+        public GameObject CameraBind;
+        public float CameraFollowingExtent = 25.0f;
+        public Vector3 CameraBindEulerAngles = new Vector3(35, 30, 0);
+        public Vector3 CameraBindPosition = Vector3.zero;
+        public bool CameraFollow;
+        public float FollowingNeedtime;
+
+        public float Speed = 15.0f;
+
+        public AudioSource Song;
+        public Rigidbody LineRigidbody;
+
+        public Vector3 TurnAround1 = new Vector3(0, 90, 0), TurnAround2 = new Vector3(0, 0, 0);
+        public bool TurnToTA1 = true;
+
+        public GameObject LineTail;
+        private GameObject nowTail;
+
+        public bool Invincible = false;
+        public AudioClip DieAudioN;
+        public GameObject DieEffect;
+
+        private bool nowPlaying = false;
+        private bool isOver = false;
+
+
+        void TurnBlock()
         {
-            transform.localEulerAngles = TurnAround2;
-            TurnToTA1 = false;
-        }
-        else
-        {
-            transform.localEulerAngles = TurnAround1;
-            TurnToTA1 = true;
-        }
-        nowTail = Instantiate(LineTail, transform.position, transform.rotation);
-    }
-
-    public bool IsGrounded()
-    {
-        return Physics.Raycast(transform.position, Vector3.down, transform.localScale.y / 2 + 0.1f);
-    }
-
-    bool Click()
-    {
-        return (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space));
-    }
-
-    //Start函数在第一帧调用
-    void Start()
-    {
-        Song.Pause();
-
-        if(TurnToTA1)
-        {
-            transform.localEulerAngles = TurnAround1;
-        }
-        else
-        {
-            transform.localEulerAngles = TurnAround2;
-        }
-
-        nowTail = Instantiate(LineTail, transform.position, transform.rotation);
-        nowPlaying = false;
-
-        CameraBind.transform.eulerAngles = CameraBindEulerAngles;
-        CameraBind.transform.localScale = new Vector3(CameraFollowingExtent, CameraFollowingExtent, CameraFollowingExtent);
-    }
-
-    //Update函数每帧调用一次
-    void Update()
-    {
-        if(nowPlaying)
-        {
-            transform.Translate(0, 0, Time.deltaTime * Speed);
-
-            if(IsGrounded())
+            if(TurnToTA1)
             {
-                if(nowTail == null)
-                {
-                    nowTail = Instantiate(LineTail, transform.position, transform.rotation);
-                }
-                nowTail.transform.Translate(0, 0, (Time.deltaTime / 2 * Speed));
-                nowTail.transform.localScale += new Vector3(0, 0, (Time.deltaTime * Speed));
-
-                if(Click())
-                {
-                    TurnBlock();
-                }
+                transform.localEulerAngles = TurnAround2;
+                TurnToTA1 = false;
             }
             else
             {
-                if(nowTail != null)
-                {
-                    nowTail = null;
-                }
+                transform.localEulerAngles = TurnAround1;
+                TurnToTA1 = true;
+            }
+            nowTail = Instantiate(LineTail, transform.position, transform.rotation);
+        }
+
+        public bool IsGrounded()
+        {
+            return Physics.Raycast(transform.position, Vector3.down, transform.localScale.y / 2 + 0.1f);
+        }
+
+        bool Click()
+        {
+            return (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space));
+        }
+
+        //Start函数在第一帧调用
+        void Start()
+        {
+            Song.Stop();
+            nowPlaying = false;
+            isOver = false;
+
+            if(TurnToTA1)
+            {
+                transform.localEulerAngles = TurnAround1;
+            }
+            else
+            {
+                transform.localEulerAngles = TurnAround2;
             }
 
-            if(CameraFollow)
+            CameraBind.transform.eulerAngles = CameraBindEulerAngles;
+            CameraBind.transform.localScale = new Vector3(CameraFollowingExtent, CameraFollowingExtent, CameraFollowingExtent);
+        }
+
+        //Update函数每帧调用一次
+        void Update()
+        {
+            if(nowPlaying && !isOver)
             {
-                CameraBind.transform.DOMove(transform.position + CameraBindPosition, FollowingNeedtime);
+                transform.Translate(0, 0, Time.deltaTime * Speed);
+
+                if(IsGrounded())
+                {
+                    if(nowTail == null)
+                    {
+                        nowTail = Instantiate(LineTail, transform.position, transform.rotation);
+                    }
+                    else
+                    {
+                        nowTail.transform.Translate(0, 0, (Time.deltaTime / 2 * Speed));
+                        nowTail.transform.localScale += new Vector3(0, 0, (Time.deltaTime * Speed));
+                    }
+
+                    if(Click())
+                    {
+                        TurnBlock();
+                    }
+                }
+                else
+                {
+                    if(nowTail != null)
+                    {
+                        nowTail = null;
+                    }
+                }
+
+                if(CameraFollow)
+                {
+                    CameraBind.transform.DOMove(transform.position + CameraBindPosition, FollowingNeedtime);
+                }
+            }
+            else if(Click())
+            {
+                nowPlaying = true;
+                nowTail = Instantiate(LineTail, transform.position, transform.rotation);
+                Song.Play();
+            }
+            else if(isOver)
+            {
+                if(Song.volume > 0)
+                {
+                    Song.volume -= 0.3f * Time.deltaTime;
+                }
+                else if(Song.volume < 0)
+                {
+                    Song.volume = 0;
+                    Song.Pause();
+                }
             }
         }
-        else if(Click())
+
+        void OnCollisionEnter(Collision collision)
         {
-            nowPlaying = true;
-            Song.Play();
+            if(nowPlaying && !isOver && !Invincible)
+            {
+                if(collision.collider.tag == "Wall")
+                {
+                    isOver = true;
+                    AudioSource.PlayClipAtPoint(DieAudioN, transform.position);
+                    LineRigidbody.constraints = RigidbodyConstraints.FreezeAll;
+                    Instantiate(DieEffect, transform.position, transform.rotation);
+                }
+            }
         }
     }
 }
